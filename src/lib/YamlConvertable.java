7877b5d78ae1960318dbe2dd644261c5d96a1503
vaskozlov <vaskozlov3940@gmail.com>
2024-02-09 19:09:19 +0300

    outdated comment removed.

--- src/lib/YamlConvertable.java
+++ src/lib/YamlConvertable.java
@@ -1,10 +1,6 @@
 package lib;
 
 public interface YamlConvertable {
-    /**
-     *
-     * @return result of building pretty string
-     */
     default String toYaml() {
         return constructYaml(new PrettyStringBuilder()).toString();
     }
